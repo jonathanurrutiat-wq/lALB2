ff4629df7854e807e01bc08facbdd07daab96973
Your Name <you@example.com>
2026-03-26 19:35:30 +0000

    new try

--- list.c
+++ list.c
@@ -89,15 +89,38 @@ void * popBack(List * list) {
 // Nota: El current debe quedar apuntando al nodo siguiente del eliminado.
 
 void * popCurrent(List * list) {
+
+
+    if(list->current != NULL)
+    {
+        void * dato = (list->current->data);
+        
+        if(lista->current->next != NULL && lista->current->prev != NULL)
+        {
+            list->current->next->prev = list->current->prev;
+            list->current->prev->next = list->current->next;
+            list->current = list->current->next;
+            
+        }else if(list->current->next == NULL && list->current->prev != NULL)
+        {
+            list->current->prev = NULL;
+            list->current = NULL;
+            
+        }else if(list->current->prev == NULL && list->current->next != NULL)
+        {
+            list->current->next->prev = NULL;
+            list->current = list->current->next:
+        }
+        else if(lista->current->next == NULL && lista->current->prev == NULL)
+        {
+            list->current = NULL;
+        }
+        
+        return dato;
+    }
     
-    void * dato = (list->current->data);
     
-    list->current->next->prev = list->current->prev;
-    list->current->prev->next = list->current->next;
-    list->current = list->current->next;
-    
-    
-    return dato;
+    return NULL;    
 }
 
 void cleanList(List * list) {
